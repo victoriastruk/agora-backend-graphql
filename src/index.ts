56f6e7205c118a7bcd32392d934195324a9318f5
Viktoriia <struk.victoria@gmail.com>
2025-10-29 23:16:49 +0200

    chore: initial project setup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
-
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+const app = new Elysia().get('/', () => 'Hello Elysia').listen(3000);
